ba44e177fc7c9d283b9eb33e810b7d099149ea7b
sayem80 <c.sayem80@gmail.com>
2021-02-26 16:20:03 -0500

    complete project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import PropTypes from 'prop-types';
+import { Box, Grid, makeStyles } from '@material-ui/core';
+import Item from './components/Item';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: theme.spacing(6),
+  }
+}))
+
+export default function App({ items }) {
+  const classes = useStyles();
+  const [activeIndex, setActiveIndex] = React.useState(undefined);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className={classes.root}>
+      <Grid container spacing={2}>
+        {items && items.map((item, index) => (
+          <Grid item xs={12} sm={6} md={3} key={index}>
+            <Item item={item} active={index === activeIndex} onClick={() => setActiveIndex(index)} />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 }
 
-export default App;
+App.defaultProps = {
+  items: [
+    {
+      title: "Lake Inniscarra, Ireland—Pyramid",
+      time: "30:53",
+      distance: 6248,
+      img: "/assets/lake-inniscarra-thumb.jpg",
+      avatar: "/assets/lake-inniscarra-trainer.jpg"
+    },
+    {
+      title: "Performance Series",
+      workoutNumber: 9,
+      img: "/assets/performance-series-thumb.jpg",
+      avatar: "/assets/performance-series-trainer.jpg"
+    },
+    {
+      title: "Slow Pulls and Fast Intervals",
+      time: "44:13",
+      distance: 9948,
+      img: "/assets/slow-pulls-thumb.jpg",
+      avatar: "/assets/slow-pulls-trainer.jpg"
+    },
+    {
+      title: "20 Minutes to Toned",
+      workoutNumber: 12,
+      img: "/assets/20-minutes-to-toned-thumb.jpg",
+      avatar: "/assets/20-minutes-to-toned-trainer.jpg"
+    },
+    {
+      title: "Charles Race, Boston, Massachusetts",
+      time: "36:22",
+      distance: 8648,
+      img: "/assets/charles-race-thumb.jpg",
+      avatar: "/assets/charles-race-trainer.jpg"
+    },
+    {
+      title: "Full-Body HIIT Series",
+      workoutNumber: 12,
+      img: "/assets/full-body-hiit-thumb.jpg",
+      avatar: "/assets/full-body-hiit-trainer.jpg"
+    },
+    {
+      title: "Kafue River, Zambia—Power Stroke Pyramid",
+      time: "36:22",
+      distance: 8648,
+      img: "/assets/charles-race-thumb.jpg",
+      avatar: "/assets/charles-race-trainer.jpg"
+    },
+    {
+      title: "Shred & Burn Series",
+      workoutNumber: 16,
+      img: "/assets/shred-and-burn-thumb.jpg",
+      avatar: "/assets/shred-and-burn-trainer.jpg"
+    },
+  ]
+}
